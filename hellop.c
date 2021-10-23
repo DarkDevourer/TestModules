6caf086896bf3b649751cb481b730ffa7b7fb59f
DarkDevourer <kuhcrazan2000@mail.ru>
2021-10-23 18:15:34 +0300

    New Module

--- hellop.c
+++ hellop.c
@@ -5,27 +5,23 @@
 MODULE_LICENSE("GPL");
 
 static char *whom = "world";
-
 module_param(whom, charp, 0644);
 MODULE_PARM_DESC(whom, "Recipient of the hello message");
-static int howmany = 1;
 
-module_param(howmany, int, 0644);
-MODULE_PARM_DESC(howmany, "Number of greetings");
+static char *time = "day";
+module_param(time, charp, 0644);
+MODULE_PARM_DESC(time, "Time");
 
 static int __init hello_init(void)
 {
-	int i;
-	for (i = 0; i < howmany; i++)
-	pr_alert("(%d) Hello, %s\n", i, whom);
+	pr_alert("Hello, %s\n! What a beautiful %s\n!", whom, time);
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_alert("Goodbye, cruel %s\n", whom);
+	pr_alert("Goodbye, cruel %s\n.", whom);
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
